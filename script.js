502ab65103b68b8f9eace473909db3c6ff5df8f3
Mane Shivam <maneshivam.a@gmail.com>
2023-05-30 00:37:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = document.querySelector('.score').textContent;
+let highScore = 0;
+
+const displayMessage= function(message){
+document.querySelector('.message').textContent = message
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  //No input
+  if (!guess) {
+    // document.querySelector('.message').textContent = '⛔ No number!';
+    displayMessage('⛔ No number!')
+  }
+
+  //Correct guess
+  else if (guess === secretNum) {
+    // document.querySelector('.message').textContent = '🎉 Correct number!';
+    displayMessage('🎉 Correct number!')
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNum;
+
+    if (score > highScore) {
+      highScore = score;
+    }
+    document.querySelector('.highscore').textContent = highScore;
+  }
+
+  //Incorrect guess
+  else if (guess !== secretNum) {
+    if (score > 1) {
+    //   document.querySelector('.message').textContent =
+    //     guess > secretNum ? '📈 Num too high!' : '📉 Num too low!';
+    displayMessage(guess > secretNum ? '📈 Num too high!' : '📉 Num too low!')
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+    }
+  }
+
+
+//Reset game
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNum = Math.trunc(Math.random() * 20) + 1;
+
+//   document.querySelector('.message').textContent = 'Start guessing...';
+ displayMessage('Start guessing...')
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+})
